d1c33ac3c474072e19431de322e980b40dfd0be7
alvarillo89 <alvaro89@correo.ugr.es>
2020-03-18 12:20:13 +0100

    Arduino test demos added

---
+++ demos/FreeRTOS_demo.ino
@@ -0,0 +1,69 @@
+#include <Arduino_FreeRTOS.h>
+
+void TaskBlink( void *pvParameters );
+void TaskSerialPrint( void *pvParameters );
+
+void setup() {
+  
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB, on LEONARDO, MICRO, YUN, and other 32u4 based boards.
+  }
+
+  // Now set up two tasks to run independently.
+  xTaskCreate(
+    TaskBlink
+    ,  "Blink"   // A name just for humans
+    ,  128  // This stack size can be checked & adjusted by reading the Stack Highwater
+    ,  NULL
+    ,  2  // Priority, with 3 (configMAX_PRIORITIES - 1) being the highest, and 0 being the lowest.
+    ,  NULL );
+
+  xTaskCreate(
+    TaskSerialPrint
+    ,  "SerialPrint"
+    ,  128  // Stack size
+    ,  NULL
+    ,  1  // Priority
+    ,  NULL );
+
+  // Now the task scheduler, which takes over control of scheduling individual tasks, is automatically started.
+}
+
+void loop()
+{
+  // Empty. Things are done in Tasks.
+}
+
+/*--------------------------------------------------*/
+/*---------------------- Tasks ---------------------*/
+/*--------------------------------------------------*/
+
+void TaskBlink(void *pvParameters)
+{
+  (void) pvParameters;
+  
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  for (;;) // A Task shall never return or exit.
+  {
+    digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+    digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+    vTaskDelay( 1000 / portTICK_PERIOD_MS ); // wait for one second
+  }
+}
+
+void TaskSerialPrint(void *pvParameters)
+{
+  (void) pvParameters;
+
+  for (;;)
+  {
+    // Print something:
+    Serial.println("IM PRINTING");
+    vTaskDelay(1);  // one tick delay (15ms) in between reads for stability
+  }
+}
